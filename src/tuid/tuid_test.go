e1dfb918e1896bf2b8ea3a96ae0c268a355c6672
David Sparks <david.sparks@gmail.com>
2016-08-14 13:38:27 -0700

    Try to make the designation between a TUID (a number) and a TUID generator (object) clearer.

--- src/tuid/tuid_test.go
+++ src/tuid/tuid_test.go
@@ -1,28 +1,23 @@
 package tuid
 
 import (
-	"log"
+	"fmt"
 	"testing"
 )
 
-func init() {
-	log.SetFlags(log.Ldate | log.Lmicroseconds | log.Lshortfile)
-}
-
 func Test_New(t *testing.T) {
 
-	//tuid.Init("N54C8R2☺")
-
-	log.Printf("Testing a valid tuid spec")
-	tuid, err := New("N54C8R2")
-	if tuid == nil || err != nil {
-		t.Errorf("Decoding valid tuid failed, err: %v, tuid: %v", err, tuid)
+	fmt.Printf("# Testing invalid tuid spec")
+	tuider, err := New("N54C8R2☺")
+	if tuider != nil || err == nil {
+		t.Errorf("Decoding invalid tuid spec succeeded")
 	}
-	log.Printf("tuid: %v", tuid)
 
-	log.Printf("Testing invalid tuid spec")
-	tuid, err = New("N54C8R2☺")
-	if err != nil && tuid != nil {
-		t.Errorf("Decoding invalid tuid succeeded, err: %v, tuid: %v", err, tuid)
+	fmt.Printf("# Testing a valid tuid spec")
+	tuider, err = New("N54C8R2")
+	if tuider == nil || err != nil {
+		t.Errorf("Decoding valid tuid spec failed, err: %v", err)
 	}
+	fmt.Printf("# tuider: %v", tuider)
+
 }
